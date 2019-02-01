b9c7b36ec2509356f4a8b60239c5c4e496f85577
CloserWU <18969946575@163.com>
2019-02-01 19:05:09 +0800

    update

--- my-project/src/components/Goods/index.js
+++ my-project/src/components/Goods/index.js
@@ -1,15 +1,65 @@
 import React from 'react';
-import { Input } from 'antd';
+import { Input, Card, Slider, Button, Col, Row, Divider, List, Avatar } from 'antd';
 import styles from './index.less';
 
 const Search = Input.Search;
 
+function onChange(value) {
+  console.log('onChange: ', value);
+}
+
+function onAfterChange(value) {
+  console.log('onAfterChange: ', value);
+}
+
+const data = [
+  {
+    title: 'Title 1',
+  },
+  {
+    title: 'Title 2',
+  },
+  {
+    title: 'Title 3',
+  },
+];
+
 export default () => (
   <div>
-    <Search
-      placeholder="input search text"
-      onSearch={value => console.log(value)}
-      style={{ width: 200 }}
-    />
+    <Card>
+      <h3>搜索</h3>
+      <Search
+        placeholder="input search text"
+        onSearch={value => console.log(value)}
+        style={{ width: 200 }}
+        className={styles.search}
+      />
+      <Divider />
+      <h3>价格区间</h3>
+      <Slider max={1000} range step={1} defaultValue={[220, 650]} onChange={onChange} onAfterChange={onAfterChange} />
+      <Row align="middle" type="flex">
+        <Col span={8}>
+          <h4 className={styles.h4}>价格0-650</h4>
+        </Col>
+        <Col span={6} offset={10}>
+          <Button type="primary" className={styles.button}>筛选</Button>
+        </Col>
+      </Row>
+      <Divider />
+      <h3>最受欢迎</h3>
+      <List
+        itemLayout="horizontal"
+        dataSource={data}
+        renderItem={item => (
+          <List.Item className={styles.list}>
+            <List.Item.Meta
+              avatar={<img alt='' src="https://gw.alipayobjects.com/zos/rmsportal/ComBAopevLwENQdKWiIn.png" width={50} height={50} />}
+              title={item.title}
+              description={<a href="https://ant.design">我要定制</a>}
+            />
+          </List.Item>
+        )}
+      />,
+    </Card>
   </div>
 );
--- my-project/src/pages/_Goods/DressA.js
+++ my-project/src/pages/_Goods/DressA.js
@@ -1,21 +1,64 @@
 import React, { Component } from 'react';
-import { Col, Row, Card } from 'antd';
+import { Col, Row, Card, Pagination } from 'antd';
 import styles from './DressA.less';
 import Goodwrap from '@/components/Goods';
 import PageHeaderWrapper from '@/components/PageHeaderWrapper';
 
+function itemRender1(current, type, originalElement) {
+  if (type === 'prev') {
+    return <a>Previous</a>;
+  } if (type === 'next') {
+    return <a>Next</a>;
+  }
+  return originalElement;
+}
+
 class DressA extends Component {
+
   render() {
     return (
       <PageHeaderWrapper>
         <Row gutter={24}>
           <Col lg={17} md={24}>
-            <Card>
-              <Goodwrap />
-              <Goodwrap />
-              <Goodwrap />
+            <Card className={styles.card}>
+              <Row>
+                <Col span={8}>
+                  <div>
+                    <img alt='' src="https://gw.alipayobjects.com/zos/rmsportal/ComBAopevLwENQdKWiIn.png" width={200} />
+                    <h3><a href="https://google.com">123</a></h3>
+                    <h3>456</h3>
+                  </div>
+                  <div>
+                    <img alt='' src="https://gw.alipayobjects.com/zos/rmsportal/ComBAopevLwENQdKWiIn.png" width={200} />
+                    <h3><a href="https://google.com">123</a></h3>
+                    <h3>456</h3>
+                  </div>
+                  <div>
+                    <img alt='' src="https://gw.alipayobjects.com/zos/rmsportal/ComBAopevLwENQdKWiIn.png" width={200} />
+                    <h3><a href="https://google.com">123</a></h3>
+                    <h3>456</h3>
+                  </div>
+                </Col>
+                <Col span={8}>
+                  <div>
+                    <img alt='' src="https://gw.alipayobjects.com/zos/rmsportal/ComBAopevLwENQdKWiIn.png" width={200} />
+                    <h3><a href="https://google.com">123</a></h3>
+                    <h3>456</h3>
+                  </div>
+                </Col>
+                <Col span={8}>
+                  <div>
+                    <img alt='' src="https://gw.alipayobjects.com/zos/rmsportal/ComBAopevLwENQdKWiIn.png" width={200} />
+                    <h3><a href="https://google.com">123</a></h3>
+                    <h3>456</h3>
+                  </div>
+                </Col>
+              </Row>
+              <Pagination className={styles.page} total={500} itemRender={itemRender1} />,
             </Card>
+
           </Col>
+
           <Col lg={7} md={24}>
             <Goodwrap />
           </Col>
