0526ec72e793efc36c4e23be4aba11ae0d3b46ff
CloserWU <18969946575@163.com>
2020-05-30 15:59:24 +0800

    .NET xPC

---
+++ 基于.NET Framework的xPC实验仿真系统/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MathWorks.xPCTarget.FrameWork;
+
+namespace ConsoleApp1
+{
+    // 首先引入xPCFramework.dll。
+    // 然后要把xpcapi.dll (x64)的放到.exe的文件夹下；把xpcapi.dll (x86) 的放到解决方案下
+    class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            // xPCTargetPC设置IP和端口
+            xPCTargetPC xPCTarget = new xPCTargetPC();
+            xPCTarget.TcpIpTargetAddress = "192.168.88.189";
+            xPCTarget.TcpIpTargetPort = "22222";
+            try
+            {   
+                // 建立连接
+                xPCTarget.Connect();
+                if (xPCTarget.IsConnected == true)
+                {
+                    Console.WriteLine(true);
+                    // 装载模型，并获取target机的应用
+                    xPCApplication application = xPCTarget.Load("C:\\Users\\Closer\\Desktop\\matlab\\xpctank1.dlm");
+                    xPCTargetScopeCollection tScopes = application.Scopes.TargetScopes;
+                    tScopes.Refresh();
+                   
+                    // 指定参数
+                    xPCParameters param = xPCTarget.Application.Parameters;
+                    // 先清空参数
+                    param.Refresh();
+                    // 获取参数的坐标和参数名
+                    xPCParameter p = param["SetPoint", "Value"];
+                    Double[] values = new Double[] { 5.0 };
+                    // 指定参数值
+                    p.SetParam(values);
+                    
+                    // 设置采样时间
+                    application.SampleTime = 0.01;
+                    // 设置结束时间 单位秒  -1是永不停止
+                    application.StopTime = 60;
+                    // target机开始运行
+                    application.Start();
+                }
+                else
+                {
+                    //Call the Disconnect methods to close the communication channel with the target PC.
+                    xPCTarget.Disconnect();
+ 
+                }
+        
+            }
+            catch (xPCException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
